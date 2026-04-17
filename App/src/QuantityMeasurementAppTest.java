238add5993f73431b66cf7822cfaafef197d98f8
praveen132006 <pm0940@srmist.edu.in>
2026-04-17 22:00:10 +0530

    UC17

--- App/src/QuantityMeasurementAppTest.java
+++ App/src/QuantityMeasurementAppTest.java
@@ -1,40 +1,29 @@
+import java.util.Arrays;
+
 public class QuantityMeasurementAppTest {
-
-    static void bubbleSort(int[] capacities) {
-        int n = capacities.length;
-
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n - 1 - i; j++) {
-
-                if (capacities[j] > capacities[j + 1]) {
-                    int temp = capacities[j];
-                    capacities[j] = capacities[j + 1];
-                    capacities[j + 1] = temp;
-                }
-            }
-        }
-    }
 
     public static void main(String[] args) {
 
         System.out.println("==================================================");
-        System.out.println(" UC16 - Manual Sorting using Bubble Sort ");
+        System.out.println(" UC17 - Sort Bogie Names Using Arrays.sort() ");
         System.out.println("==================================================\n");
 
-        int[] capacities = {72, 56, 24, 70, 60};
+        String[] bogieNames = {
+                "Sleeper",
+                "AC Chair",
+                "First Class",
+                "General",
+                "Luxury"
+        };
 
-        System.out.println("Original Capacities:");
-        for (int c : capacities) {
-            System.out.print(c + " ");
-        }
+        System.out.println("Original Bogie Names:");
+        System.out.println(Arrays.toString(bogieNames));
 
-        bubbleSort(capacities);
+        Arrays.sort(bogieNames);
 
-        System.out.println("\n\nSorted Capacities (Ascending):");
-        for (int c : capacities) {
-            System.out.print(c + " ");
-        }
+        System.out.println("\nSorted Bogie Names:");
+        System.out.println(Arrays.toString(bogieNames));
 
-        System.out.println("\n\nProgram continues...");
+        System.out.println("\nProgram continues...");
     }
 }
